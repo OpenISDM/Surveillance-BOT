aee538eaeee315abc4ba802a2423fee3cab237d2
Joe <joe@DHCP-22030.iis.sinica.edu.tw>
2020-06-08 11:33:12 +0800

    add copyright

--- src/js/components/container/menuContainer/ReportContainer.js
+++ src/js/components/container/menuContainer/ReportContainer.js
@@ -1,3 +1,39 @@
+/*
+    Copyright (c) 2020 Academia Sinica, Institute of Information Science
+
+    License:
+        GPL 3.0 : The content of this file is subject to the terms and conditions
+
+    Project Name:
+        BiDae Object Tracker (BOT)
+
+    File Name:
+        ReportContainer.js
+
+    File Description:
+        BOT UI component
+
+    Version:
+        1.0, 20200601
+
+    Abstract:
+        BeDIS uses LBeacons to deliver 3D coordinates and textual descriptions of
+        their locations to users' devices. Basically, a LBeacon is an inexpensive,
+        Bluetooth device. The 3D coordinates and location description of every 
+        LBeacon are retrieved from BeDIS (Building/environment Data and Information 
+        System) and stored locally during deployment and maintenance times. Once 
+        initialized, each LBeacon broadcasts its coordinates and location 
+        description to Bluetooth enabled user devices within its coverage area. It 
+        also scans Bluetooth low-energy devices that advertise to announced their 
+        presence and collect their Mac addresses.
+
+    Authors:
+        Tony Yeh, LT1stSoloMID@gmail.com
+        Wayne Kang, b05505028@ntu.edu.tw
+        Edward Chen, r08921a28@ntu.edu.tw
+        Joe Chou, jjoe100892@gmail.com
+*/
+
 import React, {Fragment} from 'react';
 import { reportContainerModule } from '../../../config/pageModules'
 import {
--- src/js/components/presentational/SearchResultList.js
+++ src/js/components/presentational/SearchResultList.js
@@ -1,3 +1,39 @@
+/*
+    Copyright (c) 2020 Academia Sinica, Institute of Information Science
+
+    License:
+        GPL 3.0 : The content of this file is subject to the terms and conditions
+
+    Project Name:
+        BiDae Object Tracker (BOT)
+
+    File Name:
+        SearchResultList.js
+
+    File Description:
+        BOT UI component
+
+    Version:
+        1.0, 20200601
+
+    Abstract:
+        BeDIS uses LBeacons to deliver 3D coordinates and textual descriptions of
+        their locations to users' devices. Basically, a LBeacon is an inexpensive,
+        Bluetooth device. The 3D coordinates and location description of every 
+        LBeacon are retrieved from BeDIS (Building/environment Data and Information 
+        System) and stored locally during deployment and maintenance times. Once 
+        initialized, each LBeacon broadcasts its coordinates and location 
+        description to Bluetooth enabled user devices within its coverage area. It 
+        also scans Bluetooth low-energy devices that advertise to announced their 
+        presence and collect their Mac addresses.
+
+    Authors:
+        Tony Yeh, LT1stSoloMID@gmail.com
+        Wayne Kang, b05505028@ntu.edu.tw
+        Edward Chen, r08921a28@ntu.edu.tw
+        Joe Chou, jjoe100892@gmail.com
+*/
+
 import React, { Fragment } from 'react';
 import _ from 'lodash';
 import { AppContext } from '../../context/AppContext';
@@ -11,7 +47,7 @@ import MobileSearchResultList from '../platform/mobile/MobileSearchResultList';
 import BrowserSearchResultList from '../platform/browser/BrowserSearchResultList';
 
 
-class SearchResult extends React.Component {
+class SearchResultList extends React.Component {
 
     static contextType = AppContext
 
@@ -105,4 +141,4 @@ class SearchResult extends React.Component {
     }
 }
 
-export default SearchResult
+export default SearchResultList
